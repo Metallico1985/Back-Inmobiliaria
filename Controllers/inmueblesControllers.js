3a8d34edcd3fefc6ace95512c7aae2cf1de35233
Metallico1985 <andrescardonalatorre@gmail.com>
2022-12-06 09:03:16 -0300

    Update back

--- Controllers/inmueblesControllers.js
+++ Controllers/inmueblesControllers.js
@@ -1,9 +1,11 @@
 const knex = require("../Config/bd");
+// const formidable = require('formidable');
+
 
 exports.listarInmuebles = (req, res) => {
     knex.select("*")
         .from("inmuebles")
-        .join("direcciones", { id_inmueble: "inmuebles.id_inmueble" })
+        .join("direcciones", { 'direcciones.id_inmueble': "inmuebles.id_inmueble" })
         .then((resultado) => {
             res.json(resultado);
         })
@@ -12,10 +14,57 @@ exports.listarInmuebles = (req, res) => {
         });
 };
 
+
+
+
+// exports.ingresarInmueble = async (req, res) => {
+
+//     let form = new formidable.IncomingForm();
+//     form.keepExtensions = true;
+
+//     form.parse(req, (err, fields, files) => {
+//         if (err) {
+//             return res.status(400).json({
+//                 error: "No se pudo cargar la imagen"
+//             });
+//         }
+//         const { descripcion, m2, tipo_inmueble, tipo_operacion, precio, dormitorios, direccion, pais, departamento, barrio } = fields
+
+//         try {
+//             await knex.transaction(async (trx) => {
+//                 const inmueble_nuevo = await trx('inmuebles')
+//                     .insert({
+//                         descripcion: descripcion,
+//                         m2: m2,
+//                         precio: precio,
+//                         tipo_inmueble: tipo_inmueble,
+//                         tipo_operacion: tipo_operacion,
+//                         dormitorios: dormitorios
+
+//                     }, 'id_inmueble');
+//                 console.log(inmueble_nuevo);
+
+//                 await trx('direcciones')
+//                     .insert({
+//                         direccion: direccion,
+//                         pais: pais,
+//                         departamento: departamento,
+//                         barrio: barrio,
+//                         id_inmueble: inmueble_nuevo[0].id_inmueble
+//                     });
+//             })
+//             res.json({
+//                 mensaje: "El inmueble se ha ingresado correctamente"
+//             })
+//         } catch (error) {
+//             res.status(400).json({ error: error.message });
+//         }
+//     })
+// }
+
 exports.ingresarInmueble = async (req, res) => {
 
     const { descripcion, m2, tipo_inmueble, tipo_operacion, precio, dormitorios, direccion, pais, departamento, barrio } = req.body;
-    // console.log(descripcion, m2, tipo, tipoOperacion, precio, dormitorios, direccion, pais, departamento, ciudad)
     try {
         await knex.transaction(async (trx) => {
             const inmueble_nuevo = await trx('inmuebles')
@@ -78,6 +127,29 @@ exports.modificarInmueble = async (req, res) => {
         })
         res.json({
             mensaje: "El inmueble se ha modificado correctamente"
+        })
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.eliminarInmueble = async (req, res) => {
+
+    const id = req.params.id;
+    console.log(id)
+    try {
+        await knex.transaction(async (trx) => {
+            const propiedadEliminada = await trx('direcciones')
+                .where("direcciones.id_inmueble", id)
+                .del();
+            console.log(propiedadEliminada);
+
+            await trx('inmuebles')
+                .where('inmuebles.id_inmueble', id)
+                .del();
+        })
+        res.json({
+            mensaje: "El inmueble se ha eliminado correctamente"
         })
     } catch (error) {
         res.status(400).json({ error: error.message });
--- Routes/inmuebles.js
+++ Routes/inmuebles.js
@@ -14,8 +14,8 @@ const { runValidation } = require("../Validators/index");
 
 router.get("/inmuebles/list", listarInmuebles)
 router.post('/inmuebles/nuevaPropiedad', ingresarInmueble);
-// router.get('/inmuebles/detalle/:id', runValidation, verifyToken, inmuebleById)
+router.get('/inmuebles/detalle/:id', runValidation, verifyToken, inmuebleById)
 router.put('/inmuebles/modificar/:id', runValidation, modificarInmueble);
-// router.delete('/inmuebles/eliminar/:id', runValidation, verifyToken, eliminarInmueble);
+router.delete('/inmuebles/eliminar/:id', runValidation, verifyToken, eliminarInmueble);
 
 module.exports = router;
