2cbc591a493d08e2e75d236478b90c9451b08a03
andres cardona latorre <andrescardonalatorre@gmail.com>
2023-07-04 00:30:31 -0300

    pool test

--- Config/bd.js
+++ Config/bd.js
@@ -5,6 +5,7 @@ const knex = require("knex")({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
+    pool:{min:0,max:200}, //prueba pool para elephant
     ssl: true,
   },
 });
